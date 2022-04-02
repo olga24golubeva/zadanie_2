51e998db0a8d72381998ae6e63c43e82ee6c5074
olga24golubeva <olgaxapaeva22@gmail.com>
2022-04-02 09:01:50 +0300

    Добавьте файлы проекта.

---
+++ zadanie_2/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_2
+{
+    public class kvadrat
+    {
+        /// <summary>
+        /// Вычисление квадратного уравнения
+        /// </summary>
+        /// <param name="a">коэфициент</param>
+        /// <param name="b">коэфициент</param>
+        /// <param name="c">коэфициент</param>
+        /// <param name="d">дискрименант</param>
+        /// <param name="x1">первый корень</param>
+        /// <param name="x2">второй корен</param>
+        /// <param name="message">сообщение</param>
+        public static void kvadratnoe(
+            double a, double b, double c,
+            out double d, out double x1, out double x2,
+            out string message)
+        {
+            d = x1 = x2 = double.NaN;
+
+            if (a == 0)
+            {
+                lineynoe(b, c, out x1, out message);
+            }
+            else
+            {
+                d = b * b - 4 * a * c;
+                if (d > 0)
+                {
+                    x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                    message = "Дискрименант больше нуля, 2 корня.";
+                }
+                else if (d == 0)
+                {
+                    x1 = -b / 2 / a;
+                    message = "Дискрименант равен нулю, 1 корня.";
+                }
+                else
+                {
+                    message = "Дискрименант меньше нуля, нет корней.";
+                }
+            }
+
+        }
+
+        public static void lineynoe(double k, double b,
+            out double x, out string message)
+        {
+            if (k == 0)
+            {
+                x = double.NaN;
+                if (b == 0) message = "Прямая совпадает с осью, ответ R";
+                else message = "Нет решений";
+            }
+            else
+            {
+                x = -b / k;
+                message = null;
+            }
+        }
+    }
+}
---
+++ zadanie_2/kvadratnoe.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace zadanie_2
+{
+    /// <summary>
+    /// Логика взаимодействия для kvadratnoe.xaml
+    /// </summary>
+    public partial class kvadratnoe : Window
+    {
+        public kvadratnoe()
+        {
+                InitializeComponent();
+            }
+
+            private void Button_Click(object sender, RoutedEventArgs e)
+            {
+                double a, b, c;
+                if (double.TryParse(this.a.Text, out a) && double.TryParse(this.b.Text, out b) && double.TryParse(this.c.Text, out c))
+                {
+                    double d, x1, x2;
+                    string message;
+                    kvadrat.kvadratnoe(a, b, c, out d, out x1, out x2, out message);
+                    string result = "";
+                    if (!double.IsNaN(d)) result += $"D={d}\n";
+                    if (!double.IsNaN(x1)) result += $"X1={x1}\n";
+                    if (!double.IsNaN(x2)) result += $"X2={x2}\n";
+                    if (!string.IsNullOrEmpty(message)) result += message;
+                    this.result.Text = result;
+                }
+            }
+        }
+    }
